8101a3ddb5b0eadbeeaaedccd8460688d3f0f233
cybertekschool <github@cybertekschool.com>
2019-10-18 17:10:31 -0400

    day1 and day2

---
+++ src/test/java/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,34 @@
+package tests.day1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+    public static void main(String[] args) {
+        //we have to setup webdriver based on the browser that we gonna use
+        WebDriverManager.chromedriver().setup();
+        //we need to create an object of appropriate class
+        ChromeDriver driver = new ChromeDriver();
+
+        //let's open google.com
+        //.get() method allows to open some website
+        driver.get("http://google.com");
+        //to read page title, there is method .getTitle()
+
+        //Test 1. Verify that title of the page is a "Google"
+        String actualResult = driver.getTitle();
+        String expectedResult = "Google";
+        if(actualResult.equals(expectedResult)){
+            System.out.println("Test passed");
+        }else {
+            System.out.println("Test failed");
+        }
+
+
+        //to close browser
+        //at the end of test execution
+        //if we opened a gate - we need to close it
+        driver.close();
+    }
+}
---
+++ src/test/java/tests/day2/BasicNavigation.java
@@ -0,0 +1,32 @@
+package tests.day2;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+        //if you have exception: cannot load a class
+        //that means that package name, doesn't match with location of the class itself
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        //to maximize browser
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        //we want to navigate, to the different page
+        //within same browser
+        //we not gonna open new browser or new tab
+        //URL can be passed as an object
+        //or as a string
+        //we use string
+        driver.navigate().to("http://amazon.com");
+        //if I want to comeback, to the previous page
+        driver.navigate().back();
+        //if I want to know URL of current website
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        //selenium cannot close browser automatically
+        //for this, we use method close()
+        driver.close();
+    }
+}
