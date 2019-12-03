0f39e02827e4d719df5402a730d98801744cedad
CybertekStudios1 <github@cybertekschool.com>
2019-12-03 16:49:00 -0500

    day 16 page object model, day 2

--- src/test/java/pages/CalendarEventsPage.java
+++ src/test/java/pages/CalendarEventsPage.java
@@ -1,4 +1,18 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utils.BrowserUtils;
+
 public class CalendarEventsPage extends BasePage{
+
+    @FindBy(css = "[title='Create Calendar event']")
+    public WebElement createCalendarEvent;
+
+    public void clickToCreateCalendarEvent(){
+        BrowserUtils.waitForVisibility(createCalendarEvent, 5);
+        BrowserUtils.waitForClickablility(createCalendarEvent, 5);
+        createCalendarEvent.click();
+    }
+
 }
