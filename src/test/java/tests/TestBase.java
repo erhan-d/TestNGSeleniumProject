34b10e3bc249dd30d7d4537cdd05f905c9184d42
CybertekStudios1 <github@cybertekschool.com>
2019-12-03 16:43:39 -0500

    day 16 page object model, day 2

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,6 @@ public abstract class TestBase {
     public void setup(){
         String url = ConfigurationReader.getProperty("url");
         Driver.get().get(url);
-        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod
