9b21b4a91b324f515508aef8fe27606a2ed0d71e
CybertekStudios1 <github@cybertekschool.com>
2019-12-05 11:48:59 -0500

    day 17 report and screenshot

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,11 @@
 package tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import utils.ConfigurationReader;
 import utils.Driver;
 
@@ -10,6 +14,21 @@ import utils.Driver;
 //In this way before and after methods will be the same
 //Every test class will extend testbase class
 public abstract class TestBase {
+// * ExtentReports itself does not build any reports, but allows reporters to access information, which in
+// * turn build the said reports. An example of building an HTML report and adding information to ExtentX:
+// * ExtentHtmlReporter html = new ExtentHtmlReporter("Extent.html");
+// * ExtentXReporter extentx = new ExtentXReporter("localhost");
+
+    protected ExtentReports extentReports;
+//    The ExtentHtmlReporter creates a rich standalone HTML file. It allows several
+    protected ExtentHtmlReporter extentHtmlReporter;
+//    Defines a test. You can add logs, snapshots, assign author and categories to a test and its children.
+    protected ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest(){
+
+    }
 
     @BeforeMethod
     public void setup(){
