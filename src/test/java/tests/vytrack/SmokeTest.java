e4a38b71e447453224ab25cc8ea274c9db39fe4b
cybertekschool <github@cybertekschool.com>
2019-12-06 14:42:29 -0500

    day 18 vytrack automation

--- src/test/java/tests/vytrack/SmokeTest.java
+++ src/test/java/tests/vytrack/SmokeTest.java
@@ -1,9 +1,27 @@
 package tests.vytrack;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
 import tests.TestBase;
 
 public class SmokeTest extends TestBase {
+
+    @Test(dataProvider = "navigationInfo")
+    public void smokeTest(String moduleName, String subModuleName, String pageSubTitle){
+        extentTest = extentReports.createTest("Verify that page subtitle is equals to "+pageSubTitle);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("storemanager85", "UserUser123");
+
+        loginPage.navigateTo(moduleName, subModuleName);
+
+        Assert.assertEquals(loginPage.getPageSubTitle(), pageSubTitle);
+
+        extentTest.pass("Verified that page subtitle '"+pageSubTitle+"' is displayed");
+
+    }
 
     @DataProvider(name = "navigationInfo")
     public Object[][] navigationInfo(){
